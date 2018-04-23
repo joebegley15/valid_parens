ac52f5fb45070f5b335c330a3b95f53f6741ae7f
Joe Begley <joebegley@Joes-MacBook-Air.local>
2018-04-23 12:21:13 -0400

    Added working python solution

---
+++ main.py
@@ -0,0 +1,12 @@
+def valid_parentheses(string):
+	counter = 0
+	for char in string:
+		if char == '(':
+			counter += 1
+		elif char == ')':
+			counter -= 1
+		if counter < 0:
+			return False
+	if counter == 0:
+		return True
+	return False
